6d9d1939a041e5961c9aa405f7fc5b80ff5c5613
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-04-06 17:53:57 +0200

    added some compile dependencies for android

---
+++ Sample/BandyerDemo.Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.App;
+
+namespace BandyerDemo.Droid
+{
+    [Application]
+    public class MainApplication : Application
+    {
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            String appId = "mAppId_b78542f60f697c8a56a13e579f2e66d0378ba6b3336fa75f961c6efb0e6b";
+            new Com.Bandyer.Android_sdk.BandyerSDK.Builder(this, appId);
+        }
+    }
+}
