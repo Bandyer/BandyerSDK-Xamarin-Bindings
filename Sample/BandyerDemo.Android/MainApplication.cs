cd915c310a04e23b6e74cd82b99d614829a4544a
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-04-09 10:41:19 +0200

    bandyerSdk with useSandbox useProduction methods, added some dependencies

--- Sample/BandyerDemo.Android/MainApplication.cs
+++ Sample/BandyerDemo.Android/MainApplication.cs
@@ -16,6 +16,7 @@ namespace BandyerDemo.Droid
             base.OnCreate();
             String appId = "mAppId_b78542f60f697c8a56a13e579f2e66d0378ba6b3336fa75f961c6efb0e6b";
             BandyerSDK.Builder builder = new BandyerSDK.Builder(this, appId)
+                .UseSandbox()
                 .WithCallEnabled()
                 .WithFileSharingEnabled()
                 .WithWhiteboardEnabled()
