cea63bc49c3d15560f9603ef468bbdbe7dd3be9b
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-04-15 12:34:42 +0200

    demo observer moduleObserver

--- Sample/BandyerDemo.Android/MainActivity.cs
+++ Sample/BandyerDemo.Android/MainActivity.cs
@@ -7,12 +7,18 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Com.Bandyer.Android_sdk.Client;
+using Java.Lang;
+using Android.Util;
+using Com.Bandyer.Android_sdk.Module;
 
 namespace BandyerDemo.Droid
 {
     [Activity(Label = "BandyerDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+        , IBandyerSDKClientObserver , IBandyerModuleObserver
     {
+        const string TAG = "BandyerDemo";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -41,10 +47,53 @@ namespace BandyerDemo.Droid
         }
         void initSdk()
         {
-            String userAlias = "userAlias";
+            BandyerSDKClient.Instance.AddObserver(this);
+            BandyerSDKClient.Instance.AddModuleObserver(this);
+
+            System.String userAlias = "client";
             BandyerSDKClientOptions options = new BandyerSDKClientOptions.Builder().Build();
             BandyerSDKClient.Instance.Init(userAlias, options);
             BandyerSDKClient.Instance.StartListening();
+        }
+
+        public void OnClientError(Throwable throwable)
+        {
+            Log.Debug(TAG, "OnClientError " + throwable);
+        }
+
+        public void OnClientReady()
+        {
+            Log.Debug(TAG, "OnClientReady");
+        }
+
+        public void OnClientStatusChange(BandyerSDKClientState state)
+        {
+            Log.Debug(TAG, "OnClientStatusChange " + state);
+        }
+
+        public void OnClientStopped()
+        {
+            Log.Debug(TAG, "OnClientStopped");
+        }
+
+        public void OnModuleFailed(IBandyerModule module, Throwable throwable)
+        {
+            Log.Debug(TAG, "OnModuleFailed " + module + " " + throwable);
+        }
+
+        public void OnModulePaused(IBandyerModule module)
+        {
+            Log.Debug(TAG, "OnModulePaused " + module);
+        }
+
+        public void OnModuleReady(IBandyerModule module)
+        {
+            Log.Debug(TAG, "OnModuleReady " + module);
+        }
+
+        public void OnModuleStatusChanged(IBandyerModule module, BandyerModuleStatus moduleStatus)
+        {
+            Log.Debug(TAG, "OnModuleStatusChanged " + module + " " + moduleStatus);
         }
     }
 }
