0a38023e523e40ee292839df71f8a186e069a5f9
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-04-22 17:21:25 +0200

    build sh

--- BandyerSdk/ios/build.sh
+++ BandyerSdk/ios/build.sh
@@ -9,3 +9,13 @@ rm -rf bin
 pod repo update
 pod install
 sharpie pod bind
+
+# cp -f Binding/Bandyer_ApiDefinitions.cs BandyerSdk.iOS/ApiDefinition.cs 
+# cp -f Binding/Bandyer_StructsAndEnums.cs BandyerSdk.iOS/Structs.cs 
+# cp -f Binding/SocketIO_ApiDefinitions.cs SocketIO.iOS/ApiDefinition.cs 
+# cp -f Binding/SocketIO_StructsAndEnums.cs SocketIO.iOS/Structs.cs 
+# cp -f Binding/Starscream_ApiDefinitions.cs Starscream.iOS/ApiDefinition.cs 
+# cp -f Binding/TwilioChatClient_ApiDefinitions.cs TwilioChatClient.iOS/ApiDefinition.cs 
+# cp -f Binding/TwilioChatClient_StructsAndEnums.cs TwilioChatClient.iOS/Structs.cs 
+# cp -f Binding/WebRTC_ApiDefinitions.cs WebRTC.iOS/ApiDefinition.cs 
+# cp -f Binding/WebRTC_StructsAndEnums.cs WebRTC.iOS/Structs.cs 
