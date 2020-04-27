0f2c92196e253ec2eb04fa2b3618f618bb42d074
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-04-27 17:12:37 +0200

    jsonDoc script

---
+++ jsondocs/build.sh
@@ -0,0 +1,4 @@
+python3 jsondoc.py "bandyer-android-sdk" "BandyerSdk.Android"
+cp -f BandyerSdk.Android.xml ../BandyerSdk/android/BandyerSdk.Android/bin/Debug/BandyerSdk.Android.xml 
+cp -f BandyerSdk.Android.xml ../BandyerSdk/android/BandyerSdk.Android/bin/Release/BandyerSdk.Android.xml 
+
