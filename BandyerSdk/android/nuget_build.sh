f75cba54cfe7b8d0e615b0e364158738735c9c39
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-05-04 14:59:14 +0200

    nuget build script

---
+++ BandyerSdk/android/nuget_build.sh
@@ -0,0 +1,44 @@
+msbuild /p:Configuration=Release /target:Build Airbnb.ParisAnnotation/Airbnb.ParisAnnotation.csproj
+nuget pack Airbnb.ParisAnnotation/Airbnb.ParisAnnotation.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Airbnb.Paris/Airbnb.Paris.csproj
+nuget pack Airbnb.Paris/Airbnb.Paris.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Badoo.AndroidWeakHandler.Android/Badoo.AndroidWeakHandler.Android.csproj
+nuget pack Badoo.AndroidWeakHandler.Android/Badoo.AndroidWeakHandler.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.Android/BandyerSdk.Android.Android.csproj
+nuget pack BandyerSdk.Android/BandyerSdk.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.AudioSession.Android/BandyerSdk.AudioSession.Android.csproj
+nuget pack BandyerSdk.AudioSession.Android/BandyerSdk.AudioSession.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.Chat.Android/BandyerSdk.Chat.Android.csproj
+nuget pack BandyerSdk.Chat.Android/BandyerSdk.Chat.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.Common.Android/BandyerSdk.Common.Android.csproj
+nuget pack BandyerSdk.Common.Android/BandyerSdk.Common.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.CommunicationCenter.Android/BandyerSdk.CommunicationCenter.Android.csproj
+nuget pack BandyerSdk.CommunicationCenter.Android/BandyerSdk.CommunicationCenter.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.CoreAV.Android/BandyerSdk.CoreAV.Android.csproj
+nuget pack BandyerSdk.CoreAV.Android/BandyerSdk.CoreAV.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.Design.Android/BandyerSdk.Design.Android.csproj
+nuget pack BandyerSdk.Design.Android/BandyerSdk.Design.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.Firecrasher.Android/BandyerSdk.Firecrasher.Android.csproj
+nuget pack BandyerSdk.Firecrasher.Android/BandyerSdk.Firecrasher.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.Matisse.Android/BandyerSdk.Matisse.Android.csproj
+nuget pack BandyerSdk.Matisse.Android/BandyerSdk.Matisse.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build BandyerSdk.SocketIOClient.Android/BandyerSdk.SocketIOClient.Android.csproj
+nuget pack BandyerSdk.SocketIOClient.Android/BandyerSdk.SocketIOClient.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Conscrypt.ConscryptAndroid.Android/Conscrypt.ConscryptAndroid.Android.csproj
+nuget pack Conscrypt.ConscryptAndroid.Android/Conscrypt.ConscryptAndroid.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Iceteck.Silicompressorr.Android/Iceteck.Silicompressorr.Android.csproj
+nuget pack Iceteck.Silicompressorr.Android/Iceteck.Silicompressorr.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Kotlinx.CoroutinesAndroid.Android/Kotlinx.CoroutinesAndroid.Android.csproj
+nuget pack Kotlinx.CoroutinesAndroid.Android/Kotlinx.CoroutinesAndroid.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Mikepenz.FastAdapter.Android/Mikepenz.FastAdapter.Android.csproj
+nuget pack Mikepenz.FastAdapter.Android/Mikepenz.FastAdapter.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Mikepenz.FastAdapterExtensionsDiff.Android/Mikepenz.FastAdapterExtensionsDiff.Android.csproj
+nuget pack Mikepenz.FastAdapterExtensionsDiff.Android/Mikepenz.FastAdapterExtensionsDiff.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build PermissionDispatcher.PermissionDispatcher.Android/PermissionDispatcher.PermissionDispatcher.Android.csproj
+nuget pack PermissionDispatcher.PermissionDispatcher.Android/PermissionDispatcher.PermissionDispatcher.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build PermissionDispatcher.PermissionDispatcherAnnotation.Android/PermissionDispatcher.PermissionDispatcherAnnotation.Android.csproj
+nuget pack PermissionDispatcher.PermissionDispatcherAnnotation.Android/PermissionDispatcher.PermissionDispatcherAnnotation.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Square.Picasso.Android/Square.Picasso.Android.csproj
+nuget pack Square.Picasso.Android/Square.Picasso.Android.nuspec -Verbosity detailed
+msbuild /p:Configuration=Release /target:Build Twilio.ChatAndroid.Android/Twilio.ChatAndroid.Android.csproj
+nuget pack Twilio.ChatAndroid.Android/Twilio.ChatAndroid.Android.nuspec -Verbosity detailed
