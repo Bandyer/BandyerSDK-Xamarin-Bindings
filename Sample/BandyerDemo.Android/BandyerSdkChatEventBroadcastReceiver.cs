ffb3f2c600b26f81af449426c649d405e9f4c7d9
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-05-08 11:40:24 +0200

    typo

--- Sample/BandyerDemo.Android/BandyerSdkChatEventBroadcastReceiver.cs
+++ Sample/BandyerDemo.Android/BandyerSdkChatEventBroadcastReceiver.cs
@@ -11,7 +11,7 @@ namespace BandyerDemo.Droid
     [IntentFilter(new[] { "com.bandyer.android_sdk.CHAT_EVENT_ACTION" })]
     public class BandyerSdkChatEventBroadcastReceiver : ChatEventBroadcastReceiver
     {
-        const string TAG = "BandyerSdkExceptionReceiver";
+        const string TAG = "BandyerSdkChatEventBroadcastReceiver";
 
         public override void OnChatEnded()
         {
