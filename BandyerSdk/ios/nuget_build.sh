3192d81ab8abcf2cec2daade0a027db6c31c87ce
Roberto Andreoni <roberto.andreoni@kalpa.it>
2020-05-14 16:49:57 +0200

    ios nuget build sh add nuget restore commands

--- BandyerSdk/ios/nuget_build.sh
+++ BandyerSdk/ios/nuget_build.sh
@@ -1,2 +1,3 @@
+nuget restore BandyerSdk.iOS/BandyerSdk.iOS.csproj
 msbuild /p:Configuration=Release /t:Clean /target:Build BandyerSdk.iOS/BandyerSdk.iOS.csproj
 nuget pack BandyerSdk.iOS/BandyerSdk.iOS.nuspec -Verbosity detailed
