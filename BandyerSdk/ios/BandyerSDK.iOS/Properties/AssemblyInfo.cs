dbe5c6c6c924c35f00fd69299330361a2600417b
Marco Brescianini <marco.brescianini@bandyer.com>
2020-05-15 15:54:55 +0200

    Rename BandyerSdk.iOS to BandyerSDK.iOS

---
+++ BandyerSdk/ios/BandyerSDK.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Foundation;
+
+[assembly: LinkerSafe]
+
+[assembly: AssemblyTitle("BandyerSDK")]
+[assembly: AssemblyDescription("Xamarin iOS binding for BandyerSDK native framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bandyer S.r.l.")]
+[assembly: AssemblyProduct("BandyerSDK")]
+[assembly: AssemblyCopyright("Copyright © 2020 Bandyer. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.4.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
